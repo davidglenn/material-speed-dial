f23d0ba7cc30527e69e3c5564565af5b5875397b
David Glenn <david@whitevalleysoftware.com>
2015-08-13 09:31:59 +0100

    Speed dial and demo

---
+++ scripts/speed-dial.js
@@ -0,0 +1,12 @@
+﻿$(function() {
+
+    /* support touch by opening the speed dial on click */
+    $('.speed-dial').each(function () {
+        var elm = $(this);
+        elm.find('.toggle').on('click', function () {
+            elm.children('.dials').find('a').toggleClass('pop-in');
+        });
+    });
+
+});
+
